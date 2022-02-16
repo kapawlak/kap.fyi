d28af8c49ea13483853391c00ad05e6ddf955b67
Kelly Pawlak <kapawlak@ucsb.edu>
2022-02-15 18:53:38 -0800

    init commit

---
+++ index.php
@@ -0,0 +1,55 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+
+    <title>K. A. Pawlak</title>
+  </head>
+  <body>
+    	<div class="text-center mx-auto" width="100%">
+<div class="card my-5 mx-auto" style="max-width: 740px">
+  <div class="row g-0 " height='100%'>
+    <div class="col-md-6 ">
+      <img src="me2021.jpg" class="img-fluid rounded-start" alt="...">
+    </div>
+    <div class="col-md-6">
+      <div class="card-body">
+        <h5 class="card-title">K. A. Pawlak, Ph.D.</h5>
+        <small  class="text-muted">Quantum Application Engineer</small><br>
+ <small  class="badge bg-secondary">kapawlak(at)ucsb.edu</small>
+
+<figure class="text-center my-5 mx-3">
+  <blockquote class="blockquote">
+    <p>If I knew a good quote, I'd certainly write it here.</p>
+  </blockquote>
+  <figcaption class="blockquote-footer">
+    Someone famous in <cite title="Source Title">Some Book</cite>
+  </figcaption>
+</figure>
+<a class="btn btn-primary" href="http://ilg.physics.ucsb.edu" role="button">ILG</a> 
+<a class="btn btn-primary" href="https://scholar.google.com/citations?user=NFzO_FUAAAAJ&hl=en" role="button">Publications</a>
+<a class="btn btn-primary" href="https://scholar.google.com/citations?user=NFzO_FUAAAAJ&hl=en" role="button">GitHub</a>
+ 
+
+</div>
+    </div>
+  </div>
+</div>
+	</div>
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js" integrity="sha384-eMNCOe7tC1doHpGoWe/6oMVemdAVTMs2xqW4mwXrXsW0L84Iytr2wi5v2QjrP/xp" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.min.js" integrity="sha384-cn7l7gDp0eyniUwwAZgrzD06kc/tftFf19TOAs2zVinnD/C7E91j9yyk5//jjpt/" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
